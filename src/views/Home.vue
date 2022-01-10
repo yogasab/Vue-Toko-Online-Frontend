5220392ede0fd0ecf2353de59ade4e322146789a
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-10 20:41:45 +0700

    Create carousel component

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="home">
+  <Header />
+  <Carousel />
+ </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Carousel from "../components/Carousel.vue";
+
+export default {
+ name: "Home",
+ components: {
+  Header,
+  Carousel,
+ },
+};
+</script>
