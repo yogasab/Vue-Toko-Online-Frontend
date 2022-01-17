a913d29e882e9cb618ed8a1be8eb374d10d48a17
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-17 22:36:15 +0700

    Fetching homepage with products API

--- src/components/home/MainCarousel.vue
+++ src/components/home/MainCarousel.vue
@@ -3,7 +3,7 @@
  <section class="women-banner spad">
   <div class="container-fluid">
    <div class="row">
-    <div class="col-lg-12 mt-5">
+    <div class="col-lg-12 mt-5" v-if="products">
      <carousel
       class="product-slider"
       :items="4"
@@ -11,9 +11,9 @@
       :autoplay="true"
       :dots="false"
      >
-      <div class="product-item">
+      <div class="product-item" v-for="product in products" :key="product.id">
        <div class="pi-pic">
-        <img src="img/products/women-2.jpg" alt="" />
+        <img :src="product.galleries[0].photo" alt="" />
         <ul>
          <li class="w-icon active">
           <a href="#"><i class="icon_bag_alt"></i></a>
@@ -24,80 +24,21 @@
         </ul>
        </div>
        <div class="pi-text">
-        <div class="catagory-name">Coat</div>
+        <div class="catagory-name">{{ product.type }}</div>
         <a href="#">
-         <h5>Mickey Baggy</h5>
+         <h5>{{ product.name }}</h5>
         </a>
         <div class="product-price">
-         $14.00
-         <span>$35.00</span>
+         Rp{{ product.price }}
+         <span>Rp{{ product.price * 0.8 }}</span>
         </div>
        </div>
       </div>
-      <div class="product-item">
-       <div class="pi-pic">
-        <img src="img/products/women-2.jpg" alt="" />
-        <ul>
-         <li class="w-icon active">
-          <a href="#"><i class="icon_bag_alt"></i></a>
-         </li>
-         <li class="quick-view">
-          <router-link to="/product">+ Quick View</router-link>
-         </li>
-        </ul>
-       </div>
-       <div class="pi-text">
-        <div class="catagory-name">Shoes</div>
-        <a href="#">
-         <h5>Guangzhou sweater</h5>
-        </a>
-        <div class="product-price">$13.00</div>
-       </div>
-      </div>
-      <div class="product-item">
-       <div class="pi-pic">
-        <img src="img/products/women-3.jpg" alt="" />
-        <ul>
-         <li class="w-icon active">
-          <a href="#"><i class="icon_bag_alt"></i></a>
-         </li>
-         <li class="quick-view">
-          <router-link to="/product">+ Quick View</router-link>
-         </li>
-        </ul>
-       </div>
-       <div class="pi-text">
-        <div class="catagory-name">Towel</div>
-        <a href="#">
-         <h5>Pure Pineapple</h5>
-        </a>
-        <div class="product-price">$34.00</div>
-       </div>
-      </div>
-      <div class="product-item">
-       <div class="pi-pic">
-        <img src="img/products/women-4.jpg" alt="" />
-        <ul>
-         <li class="w-icon active">
-          <a href="#"><i class="icon_bag_alt"></i></a>
-         </li>
-         <li class="quick-view">
-          <router-link to="/product">+ Quick View</router-link>
-         </li>
-         <li class="w-icon">
-          <a href="#"><i class="fa fa-random"></i></a>
-         </li>
-        </ul>
-       </div>
-       <div class="pi-text">
-        <div class="catagory-name">Towel</div>
-        <a href="#">
-         <h5>Converse Shoes</h5>
-        </a>
-        <div class="product-price">$34.00</div>
-       </div>
-      </div>
      </carousel>
+    </div>
+
+    <div class="col-lg-5" v-else>
+     <p>There are no newer products here</p>
     </div>
    </div>
   </div>
@@ -108,10 +49,30 @@
 
 <script>
 import carousel from "vue-owl-carousel";
+import axios from "axios";
 
 export default {
  components: { carousel },
  name: "MainCarousel",
+ data() {
+  return {
+   products: null,
+  };
+ },
+ methods: {
+  async getProducts() {
+   try {
+    const products = await axios.get("http://127.0.0.1:8000/api/v1/products");
+    this.products = products.data.data.data;
+    console.log(this.products);
+   } catch (error) {
+    console.log(error);
+   }
+  },
+ },
+ mounted() {
+  this.getProducts();
+ },
 };
 </script>
 
